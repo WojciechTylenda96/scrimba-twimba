d3c402066c6a77642766de1d6f9d3a5c52082d76
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2025-01-23 20:43:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ tweetBtn.addEventListener('click', function(){
    console.log(tweetInput.value)
 });
 
+document.addEventListener('click', function(e){
+   if(e.target.dataset.like){
+      handleLikeClick(e.target.dataset.like)
+   }
+   // console.log("like", e.target.dataset.like)
+});
+
+function handleLikeClick(tweetId){
+   const targetTweetObj = tweetsData.filter(function(tweet){
+      return tweet.uuid === tweetId
+   })[0]
+   targetTweetObj.likes++
+   console.log(targetTweetObj)
+};
+
 function getFeedHtml(){
    let feedHtml = ""
    tweetsData.forEach(function(tweet){
@@ -20,15 +35,15 @@ function getFeedHtml(){
                      <p class="tweet-text">${tweet.tweetText}</p>
                      <div class="tweet-details">
                         <span class="tweet-detail">
-                           <i class="fa-regular fa-comment-dots"></i>
+                           <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                            ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
-                           <i class="fa-solid fa-heart"></i>
+                           <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                            ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
-                           <i class="fa-solid fa-retweet"></i>
+                           <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                            ${tweet.retweets}
                         </span>
                      </div>   
