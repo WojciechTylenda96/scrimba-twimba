1dbbc7f97522f0e0af9954b9c0655c1e56b258ef
Wojciech <wojciech.tylenda.96@gmail.com>
2025-02-04 20:39:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid"
 
-
-const tweetInput = document.getElementById('tweet-input');
-
 document.addEventListener('click', function(e){
    if(e.target.dataset.like){
       handleLikeClick(e.target.dataset.like)
@@ -55,19 +52,23 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
-   console.log(tweetInput.value)
-   const newTweet = {
-      handle: `@Scrimba`,
-      profilePic: `images/scrimbalogo.png`,
-      likes: 0,
-      retweets: 0,
-      tweetText: tweetInput.value,
-      replies: [],
-      isLiked: false,
-      isRetweeted: false,
-      uuid: uuidv4(),
-  }
-   console.log(newTweet)
+   const tweetInput = document.getElementById('tweet-input')
+
+   if(tweetInput.value){
+      tweetsData.unshift({
+         handle: `@Scrimba`,
+         profilePic: `images/scrimbalogo.png`,
+         likes: 0,
+         retweets: 0,
+         tweetText: tweetInput.value,
+         replies: [],
+         isLiked: false,
+         isRetweeted: false,
+         uuid: uuidv4()
+     })
+      render()
+      tweetInput.value = ""
+   }
 }
 
 function getFeedHtml(){
